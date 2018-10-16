b504e0338deddcbfe0df8baac7c466bf69a7de25
charl KING <cking@c1r2s5.wethinkcode.co.za>
2018-10-16 12:46:45 +0200

    things

---
+++ install.php
@@ -0,0 +1,30 @@
+<?php
+    $servername = "localhost";
+    $username = "root";
+
+    function prompt($prompt_msg){
+        echo("<script type='text/javascript'> var answer = prompt('".$prompt_msg."'); </script>");
+
+        $answer = "<script type='text/javascript'> document.write(answer); </script>";
+        return($answer);
+    }
+    $prompt_msg = "Please enter database password:";
+    $password = prompt($prompt_msg);
+
+    // Create connection
+    $conn = new mysqli($servername, $username, "test");
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    } 
+    
+    // Create database
+    $sql = "CREATE DATABASE db_cking";
+    if ($conn->query($sql) === TRUE) {
+        echo "Database created successfully";
+    } else {
+        echo "Error creating database: " . $conn->error;
+    }
+    
+    $conn->close();
+?>
--- test.php
+++ test.php
@@ -4,7 +4,7 @@ $username = "username";
 $password = "password";
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+    $conn = new PDO("mysql:host=$servername;dbname=db_cking", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully"; 

6757888597a7a4b04a63d8b4e57d453442183012
charl KING <cking@c1r2s5.wethinkcode.co.za>
2018-10-17 13:58:30 +0200

    pdo is hard

--- login.php
+++ login.php
@@ -1,3 +1,4 @@
+<?php include('server.php'); ?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -9,13 +10,14 @@
         <h2>Login</h2>
     </div>
     <form method="post" action="login.php">
+    <?php include ('errors.php');?>
         <div class="input-group">
             <label>Username</label>
             <input type="text" name="username">
         </div>
         <div class="input-group">
             <label>Password</label>
-            <input type="password" name="password_1">
+            <input type="password" name="password">
         </div>
         <div class="input-group">
             <button type="submit" name="login" class="btn">Login</button>
--- server.php
+++ server.php
@@ -1,4 +1,6 @@
 <?php
+session_start();
+
 $servername = "localhost";
 $ad_username = "root";
 $ad_password = "test";
@@ -8,10 +10,8 @@ $username = "";
 $email = "";
 $errors = array();
 
-
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $ad_username, $ad_password);
-    // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     if (isset($_POST['register'])){
@@ -27,8 +27,8 @@ try {
         if (empty($email)){
             array_push($errors, "Email is required");
         }
-        if (empty($password_1)){
-            array_push($errors, "Password is required");
+        if (empty($username)){
+            array_push($errors, "Username is required");
         }
         if ($password_1 != $password_2){
             array_push($errors, "Passwords do not match");
@@ -36,19 +36,60 @@ try {
 
         //if no errors save user
         if(count($errors) == 0){
-            $password = hash('whirlpool', str_rot13(password_1));
+            $password = hash('whirlpool', str_rot13($password_1));
+            console.log($password) ;
             $sql = "INSERT INTO db_cking.users (username, email, `password`) 
                 VALUES ('$username','$email', '$password')";
-            echo $sql;
             $conn->exec($sql);
+            $_SESSION['username'] = $username;
+            $_SESSION['success'] = "You are logged in";
+            header('location: index.php');
         }
 
     }
-    }
-catch(PDOException $e)
-    {
+}
+catch(PDOException $e){
     echo $sql . "<br>" . $e->getMessage();
+}
+
+if (isset($_GET['logout'])){
+    session_destroy();
+    unset($_SESSION['username']);
+    header('location:index.php');
+}
+
+if (isset($_POST['login'])){
+
+    $username = ($_POST['username']);
+    $password = ($_POST['password']);
+
+    if (empty($username)){
+        array_push($errors, "Username is required");
     }
 
-$conn = null;
+    if (empty($password)){
+        array_push($errors, "Password is required");
+    }
+
+    if(count($errors) == 0){
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $ad_username, $ad_password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $password = hash('whirlpool', str_rot13($password));
+
+        $query = $conn->prepare('SELECT * FROM `db_cking.users` WHERE `username` = :usrname AND `password` = :`pswd`');
+        $query->execute(['usrname' => $username, 'pswd' => $password]);
+        $result = $query->fetch();
+        echo $result;
+        // if (mysqli_num_rows($result) == 1){
+        //     $_SESSION['username'] = $username;
+        //     $_SESSION['success'] = "You are logged in";
+        //     header('location: index.php');
+        // }
+        // else{
+        //     array_push($errors, "The username/password provided is invalid");
+        //     header('location: login.php');
+        // }
+    }
+}
+
 ?>

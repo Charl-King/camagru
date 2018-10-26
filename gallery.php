2657f7e590186940327c20f4aaa555e1144dd710
charl KING <cking@c1r2s5.wethinkcode.co.za>
2018-10-26 11:45:04 +0200

    displaying all in gallery

--- gallery.php
+++ gallery.php
@@ -8,12 +8,12 @@ $email = "";
 
 
 $conn = new PDO("mysql:host=$servername;dbname=$dbname", $ad_username, $ad_password, $opt);
-$stmt = $conn->prepare("SELECT pic FROM db_cking.pictures WHERE id < :idnb");
+$stmt = $conn->prepare("SELECT pic FROM db_cking.pictures");
 
 //$results = $stmt->fetchAll();
 //echo ("<img src =".$results[0][pic]."/>");
 
-$stmt->execute(["idnb"=>10]);
+$stmt->execute();
 $i = 0;
 while ($row = $stmt->fetch()) {
     echo("<img src =".$row[pic]."/>");
